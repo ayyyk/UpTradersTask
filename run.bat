f90d223b5b3f7e1228b6790adeab3c79fd5464ec
Andrey K <ayyyk.ru@gmail.com>
2023-03-02 00:40:24 +0300

    Django and first app are added

---
+++ run.bat
@@ -0,0 +1,4 @@
+ cd E:\html\PROJECTS\tasks\UpTradersTask
+
+manage.py runserver
+
---
+++ task.bat
@@ -0,0 +1,7 @@
+
+cd E:\html\PROJECTS\tasks\UpTradersTask
+
+cmd.exe /k poetry shell
+
+rem cmd.exe /k E:\html\myprojects\startpage\spenv\Scripts\activate.bat
+
