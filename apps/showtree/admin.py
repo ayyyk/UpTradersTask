508fed875e013d469ebe45b8507a98e82dec63f6
Andrey K <ayyyk.ru@gmail.com>
2023-03-02 20:14:56 +0300

    First working tree-like menu
    without cuctom tag
    without urls

--- apps/showtree/admin.py
+++ apps/showtree/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from .models import Tree
 
 class TreeAdmin(admin.ModelAdmin):
-    list_display = ('id','tree_name', 'name', 'full_name', 'up_elem')
+    list_display = (
+        'id','tree_name', 'name', 'full_name', 'up_elem', 'level','offset')
+    list_display_links = ('tree_name',)
     #readonly_fields = ('tree', 'link',)
-    # list_editable = ('update',)
+    list_editable = ('name', 'full_name', 'offset',)
     # search_fields = ('user',)
 
 admin.site.register(Tree, TreeAdmin)
--- apps/showtree/models.py
+++ apps/showtree/models.py
@@ -6,11 +6,11 @@ class Tree(models.Model):
     full_name = models.CharField(max_length=1000)
     up_elem = models.PositiveBigIntegerField(blank=True, null=True)
     level = models.PositiveIntegerField(default=0)
+    offset = models.PositiveIntegerField(default=0)
+
 
     class Meta:
         ordering=['full_name']
 
     def __str__(self):
         return f'{self.name}' # {self.up_elem_name}'
-
-
